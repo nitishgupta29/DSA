00b6d1e019d11abd289104927a634e00241a4c99
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-02-21 00:15:50 +0530

    Time: 1 ms, Memory: 46.1 MB - LeetHub

---
+++ move-zeroes/move-zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int c=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0)
+                nums[c++]=nums[i];
+        }
+        while(c<nums.length){
+            nums[c]=0;
+            c++;
+        }
+    }
+}
