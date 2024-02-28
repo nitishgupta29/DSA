48c1c9d41d6f641e551a2e0e3207035181ead6a6
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-02-28 20:56:31 +0530

    Time: 1 ms (99.64%), Space: 57 MB (49.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,11 +3,10 @@ class Solution {
         int sum=0;
         int gsum=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
-            if(sum<0)
-                sum=nums[i];
-            else
-               sum+=nums[i];
+            sum+=nums[i];
             gsum=Math.max(sum,gsum);
+            if(sum<0)
+                sum=0;
         }
         return gsum;
     }
