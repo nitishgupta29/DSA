a2f42534858d6d2f84de30f58107556c9a92808e
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-03-12 14:24:00 +0530

    Time: 1 ms (26.58%), Space: 41.6 MB (65.80%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -8,23 +8,23 @@ class Solution {
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        return memo(m-1,n-1,dp,obstacleGrid);
-    }
-    private int memo(int i,int j , int[][] dp , int[][] obstacleGrid){
-        if(i>=0 && j>=0 && obstacleGrid[i][j]==1){
-            return 0;
+        for(int i=0;i<m;i++){
+            int up=0;
+            int left=0;
+            for(int j=0;j<n;j++){
+               if(i>=0 && j>=0 && obstacleGrid[i][j]==1){
+                  dp[i][j]= 0;
+                   continue;
+               }
+               if(i==0 && j==0){
+                 dp[i][j]= 1;
+                  continue;
+               }
+              if(i>0) up=dp[i-1][j];
+              if(j>0) left=dp[i][j-1];
+              dp[i][j]=up+left;
+            }
         }
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int up=memo(i-1,j,dp , obstacleGrid);
-        int left=memo(i,j-1,dp,obstacleGrid);
-        return dp[i][j]=up+left;
+        return dp[m-1][n-1];
     }
 }
