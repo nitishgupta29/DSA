d3949152a9be73dd9aee8cd3a183497ad07a4fb4
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-03-16 13:40:55 +0530

    Time: 40 ms (61.09%), Space: 46.6 MB (54.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,21 +6,22 @@ class Solution {
         }
         if(totalSum%2!=0) return false;
         int target=totalSum/2;
-        int[][] dp=new int[nums.length][target+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        boolean[][] dp=new boolean[nums.length][target+1];
+        for(int i=0;i<nums.length;i++){
+            dp[i][0]=true;
         }
-        return rec(nums.length-1,target,nums,dp);
-    }
-    boolean rec(int i, int target, int[] nums, int[][] dp){
-        if(target==0) return true;
-        if(i==0) return target==nums[i];
-        if(dp[i][target]!=-1) return dp[i][target] == 0 ? false : true; 
-        boolean notTaken=rec(i-1,target,nums,dp);
-        boolean taken=false;
-        if(target>=nums[i])
-            taken=rec(i-1,target-nums[i],nums,dp);
-        dp[i][target] = notTaken || taken ? 1 : 0;
-        return taken || notTaken;
+         if (nums[0] <= target) {
+            dp[0][nums[0]] = true;
+        }
+        for(int i=1;i<nums.length;i++){
+            for(int j=1;j<=target;j++){
+                boolean notTaken=dp[i-1][j];
+                boolean taken=false;
+                if(j>=nums[i])
+                   taken=dp[i-1][j-nums[i]];
+                dp[i][j] = notTaken || taken;
+            }   
+         }
+        return dp[nums.length-1][target];
     }
 }
