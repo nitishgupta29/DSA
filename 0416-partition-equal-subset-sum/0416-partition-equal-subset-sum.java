f49ec355bdf056d24f9bf867908cf9c0edc653b7
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-03-16 14:03:50 +0530

    Time: 38 ms (66.11%), Space: 44.4 MB (82.70%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -6,22 +6,23 @@ class Solution {
         }
         if(totalSum%2!=0) return false;
         int target=totalSum/2;
-        boolean[][] dp=new boolean[nums.length][target+1];
-        for(int i=0;i<nums.length;i++){
-            dp[i][0]=true;
-        }
+        boolean[] prev=new boolean[target+1];
+        prev[0]=true;
          if (nums[0] <= target) {
-            dp[0][nums[0]] = true;
+            prev[nums[0]] = true;
         }
         for(int i=1;i<nums.length;i++){
+            boolean cur[] = new boolean[target + 1];
+            cur[0]=true;
             for(int j=1;j<=target;j++){
-                boolean notTaken=dp[i-1][j];
+                boolean notTaken=prev[j];
                 boolean taken=false;
                 if(j>=nums[i])
-                   taken=dp[i-1][j-nums[i]];
-                dp[i][j] = notTaken || taken;
-            }   
+                   taken=prev[j-nums[i]];
+                cur[j] = notTaken || taken;
+            } 
+            prev=cur;
          }
-        return dp[nums.length-1][target];
+        return prev[target];
     }
 }
