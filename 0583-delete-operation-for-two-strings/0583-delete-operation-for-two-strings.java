eb56b3a94a6360521bc6397f619f0b309b8e609b
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-04-12 12:20:35 +0530

    Time: 7 ms (83.04%), Space: 44.8 MB (18.34%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -6,16 +6,24 @@ class Solution {
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        int lcs=rec(str1,str2,n-1,m-1,dp);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1.charAt(i-1)==str2.charAt(j-1)){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        int lcs=dp[n][m];
         int diff=n+m-2*lcs;
         return diff;
-    }
-    int rec(String s,String ss,int i,int j,int[][] dp){
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s.charAt(i)==ss.charAt(j)){
-            return dp[i][j]=1+rec(s,ss,i-1,j-1,dp);
-        }
-        return dp[i][j]=Math.max(rec(s,ss,i-1,j,dp),rec(s,ss,i,j-1,dp));
     }
 }
