935d428570958febf66beb92d226fb19a4738cb0
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-04-12 12:16:00 +0530

    Time: 7 ms (83.04%), Space: 44.7 MB (47.34%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.java
@@ -1,22 +1,21 @@
 class Solution {
     public int minDistance(String str1, String str2) {
-        int n = str1.length();
-        int m = str2.length();
-        int prev[] = new int[m + 1];
-        int curr[] = new int[m + 1];
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                
-                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
-                    curr[j] = 1 + prev[j - 1];
-                    
-                } else {
-                    curr[j] = Math.max(prev[j],curr[j-1]);
-                }
-            }
-            prev=(int[])curr.clone();
+        int n=str1.length();
+        int m=str2.length();
+        int[][] dp=new int[n+1][m+1];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-        int diff=n+m-2*prev[m];
+        int lcs=rec(str1,str2,n-1,m-1,dp);
+        int diff=n+m-2*lcs;
         return diff;
+    }
+    int rec(String s,String ss,int i,int j,int[][] dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(s.charAt(i)==ss.charAt(j)){
+            return dp[i][j]=1+rec(s,ss,i-1,j-1,dp);
+        }
+        return dp[i][j]=Math.max(rec(s,ss,i-1,j,dp),rec(s,ss,i,j-1,dp));
     }
 }
