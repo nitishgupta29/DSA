da4251abf0c721e49b4bf095a89248e69ec1f06d
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-04-17 13:20:11 +0530

    Time: 5 ms (68.36%), Space: 44.7 MB (71.48%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -8,17 +8,22 @@ class Solution {
         for(int[] rows:dp){
             Arrays.fill(rows,-1);
         }
-        return rec(n-1,m-1,word1,word2,dp);
-    }
-    int rec(int i,int j,String word1,String word2,int[][] dp){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(word1.charAt(i)==word2.charAt(j)){
-            return dp[i][j]=0+rec(i-1,j-1,word1,word2,dp);
+        for(int i=0;i<=m;i++){
+            dp[0][i]=i;
         }
-        else{
-            return dp[i][j]=1+ Math.min(rec(i,j-1,word1,word2,dp), Math.min(rec(i-1,j,word1,word2,dp),rec(i-1,j-1,word1,word2,dp)));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=i;
         }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                   dp[i][j]=dp[i-1][j-1];
+                 }
+                else{
+                   dp[i][j]=1+ Math.min(dp[i][j-1], Math.min(dp[i-1][j],dp[i-1][j-1]));
+                 }
+             }
+         }
+        return dp[n][m];
     }
 }
