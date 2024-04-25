cf1e1bdcd4a3dd2786b7df5ae283b22c8b32e125
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-04-25 11:02:24 +0530

    Time: 23 ms (57.54%), Space: 44.9 MB (44.79%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,36 +1,35 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n=s.length();
-        int m=p.length();
-        int[][] dp=new int[n+1][m+1];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
+        int m=s.length();
+        int n=p.length();
+        boolean[][] dp=new boolean[n+1][m+1];
+        dp[0][0]=true;
+        for(int j=1;j<=m;j++){
+            dp[0][j]=false;
         }
-        return rec(n-1,m-1,s,p,dp);
-    }
-    public boolean rec(int i,int j,String s, String p, int[][] dp){
-        if(i<0 && j<0) return true;
-        if(j<0 && i>=0) return false;
-        if(i<0 && j>=0){
-            for(int x=0;x<=j;x++){
-                if(p.charAt(x)!='*') return false;
+        for(int i=1;i<=n;i++){
+            boolean flag=true;
+            for(int x=1;x<=i;x++){
+                if(p.charAt(x-1)!='*'){
+                    flag=false;
+                    break;
+                }
             }
-            return true;
+            dp[i][0] = flag;
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j]==1?true:false;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s.charAt(j-1)==p.charAt(i-1) || p.charAt(i-1)=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                 }
+                else if(p.charAt(i-1)=='*'){
+                    dp[i][j]= dp[i-1][j] || dp[i][j-1];
+                 }
+                else dp[i][j]=false;
+            }
         }
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
-            boolean flag = rec(i-1,j-1,s,p,dp);
-            dp[i][j] = (flag == true) ? 1 : 0;
-            return flag;
-        }
-        if(p.charAt(j)=='*'){
-            boolean flag= rec(i-1,j,s,p,dp) || rec(i,j-1,s,p,dp);
-             dp[i][j] = (flag == true) ? 1 : 0;
-            return flag;
-        }
-        dp[i][j]=0;
-        return false;
+        return dp[n][m];
     }
 }
+        
+        
