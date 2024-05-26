f3f4337820ce570c0771e0bc1e1c3d8f8927d61e
Nitish Gupta <67508948+nitishgupta29@users.noreply.github.com>
2024-05-26 16:25:22 +0530

    Time: 26 ms (27.10%), Space: 51.1 MB (18.64%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -2,19 +2,22 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m=text1.length();
         int n=text2.length();
-        int[] prev=new int[n+1];
-        int[] cur=new int[n+1];
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                if(text1.charAt(i-1)==text2.charAt(j-1)){
-                    cur[j]= 1+prev[j-1];
-                }
-                else{
-                    cur[j]=Math.max(prev[j],cur[j-1]);
-                }
-            }
-            prev = (int[]) (cur.clone());
+        int[][] dp=new int[m][n];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
         }
-        return prev[n];
+        return rec(m-1,n-1,text1,text2,dp);
+    }
+    int rec(int i,int j,String text1,String text2,int[][] dp){
+        if(i<0 || j<0){
+            return 0;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(text1.charAt(i)==text2.charAt(j)){
+            return dp[i][j]=1+rec(i-1,j-1,text1,text2,dp);
+        }
+        else{
+            return dp[i][j]=Math.max(rec(i,j-1,text1,text2,dp),rec(i-1,j,text1,text2,dp));
+        }
     }
 }
